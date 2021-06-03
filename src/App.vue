2d664dc97f5507210461713d81fd738919620b02
北极星 <1770259775@qq.com>
2021-06-03 16:01:30 +0800

    2021.6.3

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <m-header></m-header>
+  <tab></tab>
+  <router-view></router-view>
+</template>
+
+<script>
+  // eslint-disable-next-line no-tabs
+  // @/components/header/header
+  // components/header/header.vue
+import Header from '@/components/header/header'
+import Tab from '@/components/tab/tab'
+export default {
+  components: {
+    MHeader: Header,
+    Tab
+  }
+}
+</script>
